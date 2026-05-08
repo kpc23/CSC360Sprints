334cb22fa289a15b26397ec5975f0d7ed9a70aea
Karla Carrillo <karla.carrillo@centre.edu>
2026-05-07 23:15:11 -0400

    Updates on runner, sprint 3 not compiling? Need to fix connections between server and JavaFX boots i think...

--- Sprint2/src/main/java/sprint3/Runner.java
+++ Sprint2/src/main/java/sprint3/Runner.java
@@ -1,46 +1,24 @@
 package sprint3;
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sprint3model.TournamentServerModel;
-import sprint3view.ConnectToServerController;
 
 public class Runner extends Application
 {
 	@Override
 	public void start(Stage stage) throws Exception
 	{
-		try
-		{
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(ConnectToServerController.class.getResource("../sprint3view/ConnectToServerView.fxml"));
-
-			Parent view;
-			view = loader.load();
-
-			Scene s = new Scene(view);
-
 			TournamentServerModel model = new TournamentServerModel(stage);
-			ConnectToServerController cont = loader.getController();
-			cont.setModel(model);
-
-			stage.setScene(s);
+			model.showServerPicker();
+			
 			stage.show();
 
-		} catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
-
+	
 	public static void main(String[] args)
-	{
+	{		
 		launch(args);
 	}
+
 }
